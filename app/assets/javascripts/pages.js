35ce2a4a606fa5c9bdbfdeec4c212a4caf94e551
webdepman <the.web.developer.2014@gmail.com>
2014-11-17 15:24:58 +0100

    Slider principal funcionando

--- app/assets/javascripts/pages.js
+++ app/assets/javascripts/pages.js
@@ -1,2 +1,24 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+var images=new Array('/images/rockConcert.jpg','./images/rockConcert2.jpg','./images/rockConcert3.jpg', './images/rockConcert3.jpg');
+var nextimage=0;
+
+doSlideshow();
+
+function doSlideshow()
+{
+    if($('.slideshowimage').length!=0)
+    {
+        $('.slideshowimage').fadeOut(3500,function(){slideshowFadeIn();$(this).remove()});
+    }
+    else
+    {
+        slideshowFadeIn();
+    }
+}
+function slideshowFadeIn()
+{
+    $('.slideshow').prepend($('<img class="slideshowimage" src="'+images[nextimage++]+'" style="display:none">').fadeIn(3500,function(){setTimeout(doSlideshow,5000);}));
+    if(nextimage>=images.length)
+        nextimage=0;
+}
