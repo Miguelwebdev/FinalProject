a9fa55854941d0ebda3c5cd89bb010c22247f569
webdepman <the.web.developer.2014@gmail.com>
2014-11-21 12:59:37 +0100

    ultima situacion

--- app/assets/javascripts/geoloc.js
+++ app/assets/javascripts/geoloc.js
@@ -8,86 +8,34 @@ function initialize() {
   var map = new google.maps.Map(document.getElementById('map_canvas'), mapOptions);
 
 
-  /*var marker = new google.maps.Marker({
-      position: new google.maps.LatLng(40.429462, -3.630941),
-      map: map,
-      title: 'Hello World!'
-  });*/
-
-
-  var iconBase = 'http://maps.google.com/mapfiles/kml/pushpin/';
   
-  var fnac = [
-        ['Nirvana', 47.610584,  -122.331043, 1],
-        ['Paseo de la Castellana 79', 40.447799, -3.692580, 2],
-        ['Paseo de la Finca 1', 40.418731, -3.800636, 3],
-        ];
-  var markerF, i;
 
-    for (i = 0; i < fnac.length; i++) {  
-      markerF = new google.maps.Marker({
-        position: new google.maps.LatLng(fnac[i][1], fnac[i][2],fnac[i][3]),
-        map: map,
-        title: 'Fnac',
-        icon: iconBase + 'ylw-pushpin.png'
-      })};
 
-  var casalibro = [
-          ['calle Maestro Victoria 3',  40.418431, -3.705542],
-          ['calle Gran Via 9', 40.419907, -3.703012],
-          ['calle Fuencarral 119',  40.430388, -3.702962],
-          ['calle alcala 117',  40.424650, -3.676378]
-        ];
-  var markerCL, i;
-
-    for (i = 0; i < casalibro.length; i++) {  
-      markerCL = new google.maps.Marker({
-        position: new google.maps.LatLng(casalibro[i][1], casalibro[i][2],casalibro[i][3]),
-        map: map,
-        title: 'La Casa del Libro',
-        icon: iconBase + 'grn-pushpin.png'
-      })};
-
-var bookcenter = [
-          ['calle luchana', 40.429625, -3.701239],
-          ['c.c. shopping zielo', 40.441536, -3.784008],
-          ['calle princesa 57',   40.431096, -3.716625],
-          ['c.c gran plaza 2',  40.491551, -3.899110]
-        ];
-  var markerBC, i;
-
-    for (i = 0; i < bookcenter.length; i++) {  
-      markerBC = new google.maps.Marker({
-        position: new google.maps.LatLng(bookcenter[i][1], bookcenter[i][2],bookcenter[i][3]),
-        map: map,
-        title: 'Book Center',
-        icon: iconBase + 'red-pushpin.png'
-      })};
 
       var iconBase2 = 'http://maps.google.com/mapfiles/kml/paddle/';
       var Anaya = new google.maps.Marker({
-          position: new google.maps.LatLng(40.448209, -3.629770),
+          position: new google.maps.LatLng(47.605821, -122.331786),
           map: map,
           icon: iconBase2 + 'wht-blank.png',
-          title: 'Editorial Anaya'
+          title: 'Nirvana'
       });
       var Salamandra = new google.maps.Marker({
-          position: new google.maps.LatLng(41.393649, 2.185291),
+          position: new google.maps.LatLng(32.721207, -117.174814),
           map: map,
           icon: iconBase2 + 'grn-blank.png',
-          title: 'Editorial Salamandra'
+          title: 'Stone Temple Pilots'
       });
       var Gigamesh = new google.maps.Marker({
-          position: new google.maps.LatLng(41.392091, 2.177613),
+          position: new google.maps.LatLng(51.517747, -0.129751),
           map: map,
           icon: iconBase2 + 'pink-blank.png',
-          title: 'Editorial Gigamesh'
+          title: 'Bush'
       });
       var Akal = new google.maps.Marker({
-          position: new google.maps.LatLng(40.614352, -3.709112),
+          position: new google.maps.LatLng(-32.942444, 151.751506),
           map: map,
           icon: iconBase2 + 'purple-blank.png',
-          title: 'Editorial Akal'
+          title: 'Silverchair'
       });
 
 
@@ -99,7 +47,7 @@ var bookcenter = [
         var marker = new google.maps.Marker({
           position: pos,
           map: map,
-          title: 'Hola paco'
+          title: 'Ahora estás aquí'
       });
 
       map.setCenter(pos);
