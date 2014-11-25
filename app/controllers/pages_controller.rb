34641f540e5432d9d8c4abf94443cf259e65212b
webdepman <the.web.developer.2014@gmail.com>
2014-11-25 15:47:15 +0100

    Cerca del final

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -4,22 +4,26 @@ class PagesController < ApplicationController
   end
 
   def grunge
+    @travels = current_user.travels
     @travel = Travel.find_by(travel:"Grunge")
     render layout: 'grayscaleFather'
   end
 
   def rock
+    @travels = current_user.travels
     @travel = Travel.find_by(travel:"Rock")
     render layout: 'grayscaleFather2'
   end
 
 
   def pop
+    @travels = current_user.travels
     @travel = Travel.find_by(travel:"Pop")
     render layout: 'grayscaleFather3'
   end
   
   def jazz
+     @travels = current_user.travels
      @travel = Travel.find_by(travel:"Jazz")
      render layout: 'grayscaleFather4'
   end
